71d1bbe8c14667490102827e45084d587b5d3207
黃智輝 <cpmoviephil@gmail.com>
2019-12-19 14:46:56 +0800

    dict2

---
+++ dict2.py
@@ -0,0 +1,11 @@
+user1 = {
+    'weapons': 'shotgun',
+    'life': 2,
+    'level': 20,
+}
+
+print(user1.get('age', 23))
+print(user1)
+user2 = dict(name='george', weapons='Words', age='54')
+print(user2)
+print('age' in user2)
