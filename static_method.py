61b916ef30e70526db0063cf046fc2a71f265526
黃智輝 <cpmoviephil@gmail.com>
2020-02-25 17:49:08 +0800

    static method修正

--- static_method.py
+++ static_method.py
@@ -15,9 +15,18 @@ class PlayerCharacter:
     def number_plus(number1, number2):
         return number1 + number2
 
+    @staticmethod
+    def number_minus(self, number1, number2):
+        self.number = number1-number2
+        return self.number
+
 
 print(f'static method: {PlayerCharacter.number_plus(1, 2)}')
 player1 = PlayerCharacter('Phil')
 print(f'player1\'s name: {player1.name}')
 player1.alternative_init('Jordon', 23)
 print(f're init player1: player1\'s name, {player1.name}, player1\'s age, {player1}')
+player2 = PlayerCharacter('Kobe')
+print(player2.number_plus(1, 2))
+print(player2.number_minus(player2, 20, 1))
+print(player2.number)
