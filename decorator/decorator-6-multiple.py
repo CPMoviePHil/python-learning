e97e3c9b235289680f8a4da65cf5d228f7555605
黃智輝 <cpmoviephil@gmail.com>
2020-03-11 16:30:07 +0800

    雙層Decorator修正

--- decorator/decorator-6-multiple.py
+++ decorator/decorator-6-multiple.py
@@ -10,10 +10,12 @@ def print_func_name(fn):
 
 def time_measure(fn):
     def wrapper_2():
-        t1 = time()
+        print(f'{fn.__name__} is executing!!')
         fn()
-        t2 = time()
-        print(f'{fn.__name__} take {t2-t1} seconds to perform')
+        # t1 = time()
+        # fn()
+        # t2 = time()
+        # print(f'{fn.__name__} take {t2-t1} seconds to perform')
     return wrapper_2
 
 
@@ -27,5 +29,12 @@ def useless_func():
 
 useless_func()
 # useless_func = time_measure(print_func_name(useless_func))
-# useless_func會先吃到print_func_name此decorator等定義
-# 然後才吃到time_measure等decorator的定義
+# useless()
+# 執行useless()就是執行time_measure() -> wrapper2()
+# wrapper2() 裡面fn()會print_func_name當參數執行
+# print_func_name() -> wrapper1()
+# wrapper1()會執行fn()也就是useless_func
+# 所以print的順序會是
+# wrapper1 is executing!(因為wrapper2的fn是吃到print_func_name也就是wrapper1)
+# Function Executing right now: useless_func(因為wrapper1的fn吃到的是useless_func)
+# 然後最後print i'm kinda useless
